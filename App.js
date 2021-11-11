02c0fb69c47b35cb06dc6bd67d512d3891986fbd
rlterry00 <chuckterry00@gmail.com>
2021-11-10 19:19:59 -0500

    set up server

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { avatarLinks } from "./config/avatarLinks";
+import React, { useState, useCallback, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { io } from "socket.io-client";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Mom I took out the trash, can you approve me in Penny Bank so I can watch Youtube?",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: avatarLinks[0].avatarLink,
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config/avatarLinks.js
@@ -0,0 +1,402 @@
+export let avatarLinks = [
+  {
+    avatarID: 1,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_01.png",
+    male: true,
+  },
+  {
+    avatarID: 2,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_02.png",
+    male: true,
+  },
+  {
+    avatarID: 3,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_03.png",
+    male: true,
+  },
+  {
+    avatarID: 4,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_04.png",
+    male: true,
+  },
+  {
+    avatarID: 5,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_05.png",
+    male: true,
+  },
+  {
+    avatarID: 6,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_06.png",
+    male: true,
+  },
+  {
+    avatarID: 7,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_07.png",
+    male: true,
+  },
+  {
+    avatarID: 8,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_08.png",
+    male: true,
+  },
+  {
+    avatarID: 9,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_09.png",
+    male: true,
+  },
+  {
+    avatarID: 10,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_10.png",
+    male: true,
+  },
+  {
+    avatarID: 11,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_11.png",
+    male: true,
+  },
+  {
+    avatarID: 12,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_12.png",
+    male: true,
+  },
+  {
+    avatarID: 13,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_13.png",
+    male: true,
+  },
+  {
+    avatarID: 14,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_14.png",
+    male: true,
+  },
+  {
+    avatarID: 15,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_15.png",
+    male: true,
+  },
+  {
+    avatarID: 16,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_16.png",
+    male: true,
+  },
+  {
+    avatarID: 17,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_17.png",
+    male: true,
+  },
+  {
+    avatarID: 18,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_18.png",
+    male: true,
+  },
+  {
+    avatarID: 19,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_19.png",
+    male: true,
+  },
+  {
+    avatarID: 20,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_20.png",
+    male: true,
+  },
+  {
+    avatarID: 21,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_21.png",
+    male: true,
+  },
+  {
+    avatarID: 22,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_22.png",
+    male: true,
+  },
+  {
+    avatarID: 23,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_23.png",
+    male: true,
+  },
+  {
+    avatarID: 24,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_24.png",
+    male: true,
+  },
+  {
+    avatarID: 25,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_25.png",
+    male: true,
+  },
+  {
+    avatarID: 26,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_26.png",
+    male: true,
+  },
+  {
+    avatarID: 27,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_27.png",
+    male: true,
+  },
+  {
+    avatarID: 28,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_28.png",
+    male: true,
+  },
+  {
+    avatarID: 29,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_29.png",
+    male: true,
+  },
+  {
+    avatarID: 30,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_30.png",
+    male: true,
+  },
+  {
+    avatarID: 31,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_31.png",
+    male: true,
+  },
+  {
+    avatarID: 32,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_32.png",
+    male: true,
+  },
+  {
+    avatarID: 33,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_33.png",
+    male: true,
+  },
+  {
+    avatarID: 34,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_34.png",
+    male: true,
+  },
+  {
+    avatarID: 35,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_35.png",
+    male: true,
+  },
+  {
+    avatarID: 36,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_36.png",
+    male: true,
+  },
+  {
+    avatarID: 37,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_37.png",
+    male: true,
+  },
+  {
+    avatarID: 38,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_38.png",
+    male: true,
+  },
+  {
+    avatarID: 39,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_39.png",
+    male: true,
+  },
+  {
+    avatarID: "40",
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/BoyAvatar_40.png",
+    male: true,
+  },
+  {
+    avatarID: 41,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_01.png",
+    male: false,
+  },
+  {
+    avatarID: 42,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_02.png",
+    male: false,
+  },
+  {
+    avatarID: 43,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_03.png",
+    male: false,
+  },
+  {
+    avatarID: 44,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_04.png",
+    male: false,
+  },
+  {
+    avatarID: 45,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_05.png",
+    male: false,
+  },
+  {
+    avatarID: 46,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_06.png",
+    male: false,
+  },
+  {
+    avatarID: 47,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_07.png",
+    male: false,
+  },
+  {
+    avatarID: 48,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_08.png",
+    male: false,
+  },
+  {
+    avatarID: 49,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_09.png",
+    male: false,
+  },
+  {
+    avatarID: 50,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_10.png",
+    male: false,
+  },
+  {
+    avatarID: 51,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_11.png",
+    male: false,
+  },
+  {
+    avatarID: 52,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_12.png",
+    male: false,
+  },
+  {
+    avatarID: 53,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_13.png",
+    male: false,
+  },
+  {
+    avatarID: 54,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_14.png",
+    male: false,
+  },
+  {
+    avatarID: 55,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_15.png",
+    male: false,
+  },
+  {
+    avatarID: 56,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_16.png",
+    male: false,
+  },
+  {
+    avatarID: 57,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_17.png",
+    male: false,
+  },
+  {
+    avatarID: 58,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_18.png",
+    male: false,
+  },
+  {
+    avatarID: 59,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_19.png",
+    male: false,
+  },
+  {
+    avatarID: 60,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_20.png",
+    male: false,
+  },
+  {
+    avatarID: 61,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_21.png",
+    male: false,
+  },
+  {
+    avatarID: 62,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_22.png",
+    male: false,
+  },
+  {
+    avatarID: 63,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_23.png",
+    male: false,
+  },
+  {
+    avatarID: 64,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_24.png",
+    male: false,
+  },
+  {
+    avatarID: 65,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_25.png",
+    male: false,
+  },
+  {
+    avatarID: 66,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_26.png",
+    male: false,
+  },
+  {
+    avatarID: 67,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_27.png",
+    male: false,
+  },
+  {
+    avatarID: 68,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_28.png",
+    male: false,
+  },
+  {
+    avatarID: 69,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_29.png",
+    male: false,
+  },
+  {
+    avatarID: 70,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_30.png",
+    male: false,
+  },
+  {
+    avatarID: 71,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_31.png",
+    male: false,
+  },
+  {
+    avatarID: 72,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_32.png",
+    male: false,
+  },
+  {
+    avatarID: 73,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_33.png",
+    male: false,
+  },
+  {
+    avatarID: 74,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_34.png",
+    male: false,
+  },
+  {
+    avatarID: 75,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_35.png",
+    male: false,
+  },
+  {
+    avatarID: 76,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_36.png",
+    male: false,
+  },
+  {
+    avatarID: 77,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_37.png",
+    male: false,
+  },
+  {
+    avatarID: 78,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_38.png",
+    male: false,
+  },
+  {
+    avatarID: 79,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_39.png",
+    male: false,
+  },
+  {
+    avatarID: 80,
+    avatarLink: "https://pennybank-avatars.s3.amazonaws.com/GirlAvatar_40.png",
+    male: false,
+  },
+];
